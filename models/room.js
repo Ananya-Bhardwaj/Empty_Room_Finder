e3e6272aba7014859b32fed0bca4463844c2611a
SonaVarshney <sonavarshney29@gmail.com>
2024-03-19 17:03:11 +0530

    Added Room Schema

---
+++ models/room.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const RoomSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  day: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      slots: [
+        {
+          time: {
+            type: String,
+            required: true,
+          },
+          batch: {
+            type: String,
+            required: true,
+          },
+          subject: {
+            type: String,
+            required: true,
+          },
+          teacher: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+    },
+  ],
+});
