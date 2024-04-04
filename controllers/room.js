cdfce3658340d08d5ca89950ab9d1a2dc2b6bd8a
SonaVarshney <sonavarshney29@gmail.com>
2024-04-04 15:21:40 +0530

    controllers of room added

--- controllers/room.js
+++ controllers/room.js
@@ -1,5 +1,27 @@
-//get all (room timetable)
-//get all rooms by day 
-//patch room entries slot wise 
-//post empty default wala 
+//patch room entries slot wise
+//post empty default wala
 //book room (patch request)
+
+import express from "express";
+import Room from "../models/room";
+
+//get all (room timetable) - by room
+
+export const getAllRooms = async (req, res) => {
+  try {
+    let rooms = await Room.find({ name: req.params.name });
+    res.send(rooms);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+//get all rooms by day
+export const getAllRoomsByDay = async (req, res) => {
+  try {
+    let rooms = await Room.find({ day: req.params.day });
+    res.send(rooms);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
