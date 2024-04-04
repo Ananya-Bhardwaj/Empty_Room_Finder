e095781be713547c0c337f0a112d3480c57a53ae
ananya-bhardwaj <ananya.bhar@gmail.com>
2024-04-04 12:19:31 +0530

    Changed Models

--- models/class.js
+++ models/class.js
@@ -5,30 +5,41 @@ const ClassSchema = mongoose.Schema({
         type: String, 
         required: true
     }, 
-    day: [
+    day: 
     {
-        name: {
-            type: String, 
-        }, 
-        slots: [
-            {
-                time: {
-                    type: String
-                },  
-                subject: {
-                    type: String 
-                }, 
-                teacher: {
-                    type: String
-                }, 
-                room: {
-                    type: mongoose.Schema.Types.ObjectId, 
-                    ref: 'Room'
-                }
-            }
-        ]
-    }]
-}
-)
+        type: String, 
+        required: true  
+    }, 
+    room: 
+    {
+        type: Object.Schema.Types.ObjectId, 
+        required: true, 
+        ref: "Room"
+    }, 
+    1: {
+        type: String
+    }, 
+    2: {
+        type: String
+    }, 
+    3: {
+        type: String
+    }, 
+    4: {
+        type: String
+    }, 
+    5: {
+        type: String
+    }, 
+    6: {
+        type: String
+    }, 
+    7: {
+        type: String
+    }, 
+    8: {
+        type: String
+    }
+})
 
 export default mongoose.model("Class", ClassSchema);
